84307d0f9b0f7bbfa43fa2c57c7795c78d191e48
Ruslan <Ruslan18@.ru>
2022-10-10 16:35:41 +0500

    final

---
+++ nav.js
@@ -0,0 +1,12 @@
+var years = +prompt('Введите свой возраст');
+if (years > 0 && years <=18) {
+    alert('Вы ещё молоды, Вам нужно учиться')
+}else if (years > 18 && years <=50) {
+    alert('Вам нужно работать')
+}else if (years > 50 && years <=59) {
+    alert('Вам скоро на пенсию')
+}else if (years > 59 && years <=100) {
+    alert('Вы пенсионер')
+}else{
+    alert('Что-то пошло не так')
+}
